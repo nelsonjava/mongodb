1ae09a562d77fc3fd5f036611a1d5e381b16f2ba
naif <nelsonjava@gmail.com>
2013-08-29 21:54:12 -0500

    first commit

---
+++ GridFS/README.md
@@ -0,0 +1,5 @@
+git init
+git add README.md
+git commit -m 'first commit'
+git remote add origin https://github.com/nelsonjava/mongo.git
+git push origin master
---
+++ mongo/src/main/java/com/mycompany/demo/App.java
@@ -0,0 +1,68 @@
+package com.mycompany.demo;
+
+// Demo
+
+import com.mongodb.Mongo;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.WriteConcern;
+import java.util.Date;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    Mongo conn;
+    try {
+      conn = new Mongo("localhost", 27017);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    
+    WriteConcern w = new WriteConcern( 1, 2000 );
+    conn.setWriteConcern( w );
+
+    DB db = conn.getDB( "crawler" );
+    DBCollection coll = db.getCollection( "sites" );
+
+    DBObject doc = new BasicDBObject();
+    String[] tags = { "database", "open-source" };
+
+    doc.put("url", "org.mongodb");
+    doc.put("tags", tags);
+
+    DBObject attrs = new BasicDBObject();
+    attrs.put( "lastAccess", new Date() );
+    attrs.put( "pingtime", 20 );
+
+    doc.put( "attrs", attrs );
+
+    coll.insert(doc);
+
+    System.out.println( "Initial document:\n" );
+    System.out.println( doc.toString() );
+
+    System.out.println( "Updating pingtime...\n" );
+    coll.update( new BasicDBObject( "_id", doc.get("_id") ),
+       new BasicDBObject( "$set", new BasicDBObject( "pingtime", 30 ) ) );
+
+    DBCursor cursor = coll.find();
+
+    System.out.println( "After update\n" );
+    System.out.println( cursor.next().toString() );
+
+    System.out.println( "Number of site documents: " + coll.count() );
+
+    System.out.println( "Removing documents...\n" );
+    coll.remove( new BasicDBObject() );
+
+
+}
+
+}
+
+
+
